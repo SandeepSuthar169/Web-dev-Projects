ab4b3cfd5f2461636a8b7c3c77a64a7b3a76efb8
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-09-19 08:34:23 +0530

    temprature calculator

--- 15_Temperature_Converter/script.js
+++ 15_Temperature_Converter/script.js
@@ -4,22 +4,28 @@ const kelvin = document.getElementById('kelvin')
 const input = document.querySelector("input")
 
 
-function temperatoreConvertor(event){
+function computeTemp(event){
     let tempEvent =  parseFloat(event.target.value)
     console.log(tempEvent);
+
+    if(isNaN(tempEvent)){
+        celsius.value = ""
+        fahrenheit.value = ""
+        kelvin.value = ""
+        return
+    }
     
     if (event.target.name === "celsius") {
         fahrenheit.value = tempEvent * 1.8 + 32  
         kelvin.value = tempEvent + 273.15      
+
     } else if (event.target.name === "fahrenheit"){
         celsius.value = tempEvent - 32 * 0.556
         kelvin.value = 1.8 * (tempEvent - 273.15) + 32
-    } else if (event.target.value === "kelvin"){
+    
+    } else if (event.target.name === "kelvin"){
         celsius.value = tempEvent - 273.15
-        fahrenheit.value = (tempEvent - 32) / 1.799 + 273.15
+        fahrenheit.value = (tempEvent - 273.32) * 1.8 + 32
     } 
-
 }
 
-
-// input.addEventListener("click", temperatoreConvertor)
