2e8f8eadfd392e573f49706640fe7cd369093976
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-09-22 04:42:57 +0530

    count number project :

--- 21_Count_Number/script.js
+++ 21_Count_Number/script.js
@@ -1,13 +1,27 @@
 const value = document.querySelector('.value')
 const decrease = document.querySelector('.decrease')
-const increase = document.querySelector('increase')
-const reset = document.querySelector('reset')
+const increase = document.querySelector('.increase')
+const reset = document.querySelector('.reset')
 
-let a = 0
-function decreaseValue(){
+let a = 1
+function increaseValue(){
     let b = a++
     value.innerHTML = b
 }
+increase.addEventListener('click', increaseValue)
 
+
+
+function decreaseValue(){
+    let b = a--
+    value.innerHTML = b
+}
 decrease.addEventListener('click', decreaseValue)
 
+
+function resetValue(){
+    value.innerHTML = 0
+}
+
+
+reset.addEventListener('click', resetValue)
