6bfab91b386d8a3f3f8cba5a169ac8124d88d227
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-11-12 23:58:14 +0530

    updateBook controller updated:

--- big-proj/NODE - proj/Book Bazaar backend/src/controllers/books.controllers.js
+++ big-proj/NODE - proj/Book Bazaar backend/src/controllers/books.controllers.js
@@ -2,6 +2,7 @@ import { Books } from "../models/books.models.js"
 import { ApiError } from "../utils/apiError.js"
 import { asyncHandler} from "../utils/asyncHandler.js"
 import { ApiResponse }  from "../utils/apiResponse.js"
+import { BooksStatusEnum } from "../utils/constants.js"
 
 
 
@@ -46,21 +47,82 @@ const addBook =  asyncHandler(async (req, res) => {
 })
 
 const getBookInfo =  asyncHandler(async (req, res) => {
+
+
+    // console.log(req.params);
+    const { id } = await req.params
+
+    // console.log(id);
     
-
-
+    if(!id){ 
+        throw new ApiError(404, "book id is not found") 
+    } 
+    
+    const  book  = await Books.findById(id).populate("name autharName price stock publishDate ISBN averageRating")
+    
+    if(!book){ 
+        throw new ApiError(404, "book is not found") 
+    } 
+    //3. return success
+    return res.status(201).json(new ApiResponse(
+        200,
+        book,
+        "get book info successfully"
+    ))
 })
 
 
-const getSingleBook =  asyncHandler(async (req, res) => {
 
-
-})
 
 
 const updateBook =  asyncHandler(async (req, res) => {
+    const { id } = await req.params
+
+    if(!id) throw new ApiError(401, "book id not found")
+    
+    const {price, name, stock, averageRating, totalReviews, status} = req.body
+    console.log(price);
+    console.log(name);
+    console.log(stock);
+    console.log(averageRating);
+    console.log(totalReviews);
+    console.log(status);
+    
+    console.log("==========status========");
+
+    if(price === undefined ||name === undefined||  stock === undefined || averageRating === undefined || totalReviews === undefined || status === undefined) throw new ApiError(401, "All book fileds are required")
+    
+    console.log("==========status========");
 
 
+    if(!Object.values(BooksStatusEnum).includes(status)){
+        throw new ApiError(400, "Invalid status")
+    }
+
+    const UpdateBooksValue = await Books.findByIdAndUpdate(
+        id,
+        {   
+            name,
+            price,
+            stock,
+            averageRating,
+            totalReviews,
+            status
+        },
+        { new: true, runValidators: true }
+    )
+    if(!UpdateBooksValue) throw new ApiError(401, "book not update")
+
+    console.log(UpdateBooksValue);
+        
+
+    return res.status(200).json(new ApiResponse(
+        200,
+        {
+            UpdateBooksValue,
+        },
+        "Update book successfully!"
+    ))
 })
 
 
@@ -73,5 +135,7 @@ const deleteBook =  asyncHandler(async (req, res) => {
 
 
 export { 
-    addBook
+    addBook,
+    getBookInfo,
+    updateBook
 }
--- big-proj/NODE - proj/Book Bazaar backend/src/routes/book.routes.js
+++ big-proj/NODE - proj/Book Bazaar backend/src/routes/book.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express"
-import { addBook } from "../controllers/books.controllers.js"
+import { addBook, getBookInfo, updateBook } from "../controllers/books.controllers.js"
 
 const router = Router()
 
 
 router.post("/BookAdd", addBook)
+router.get("/:id", getBookInfo)
+router.post("/:id/status", updateBook)
 
 export default router
